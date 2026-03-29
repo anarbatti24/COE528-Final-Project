95fc14bab7d9bb7598d67bdba88af6f56bc21580
Aniket Arbatti <85314749+anarbatti24@users.noreply.github.com>
2026-03-28 21:19:01 -0400

    added comments

--- src/bookstore/Owner.java
+++ src/bookstore/Owner.java
@@ -14,8 +14,16 @@ public class Owner {
     private static final String password = "admin";
     private static Owner instance;
 
+    /**
+     * Constructor method for the owner
+     * @param username The username of the owner -> "admin" (as per spec)
+     * @param password The password of the owner -> "admin" (as per spec)
+     */
     private Owner(String username, String password) {}
 
+    /**
+     * Can only have one owner, so we used Singleton Design Pattern
+     */
     public static Owner getInstance() {
         if (instance == null) {
             instance = new Owner(username, password);
@@ -23,6 +31,9 @@ public class Owner {
         return instance;
     }
 
+    /**
+     * Owner method capability functions
+     */
     public static void addBook(Book b) { inventory.add(b); }
     public static void deleteBook(Book b) { inventory.remove(b); }
     public static void addCustomer(Customer c) { customers.add(c); }
